597a8c01a8ae331cd496ad0e8650b405bfd36193
amanikitutocd.. <akituto@gmail.com>
2024-05-22 04:03:11 +0300

    completed lab

--- lib/car.py
+++ lib/car.py
@@ -2,3 +2,9 @@ from vehicle import Vehicle
 
 class Car(Vehicle):
     pass
+# lib/car.py
+from vehicle import Vehicle
+
+class Car(Vehicle):
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
